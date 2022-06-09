5b087c724ac404701eae72b6619fcca179d5d302
Aditya Wasan <adityawasan55@gmail.com>
2022-06-09 12:28:51 +0530

    init: add initial app
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

---
+++ app/src/main/java/dev/skrilltrax/epifiexcercise/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.skrilltrax.epifiexcercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
